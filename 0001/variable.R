57dc9d37778b48438b494f5cfd52a65854d8fa92
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-03-26 12:20:16 +0900

    Create variable.R

---
+++ 0001/variable.R
@@ -0,0 +1,48 @@
+a <- 1
+a
+
+b <- 2
+b
+
+c <- 3
+c
+
+d <- 3.5
+d
+
+a + b
+a + b + c
+4 / b
+5 * b
+
+v1 <- c(1,2,3,4,5) # 1,2,3,4,5 
+v1
+
+v2 <- c(1:10) # 1~10
+v2
+
+v3 <- seq(1, 10) # 1~10 c(1:10)과 같음
+v3
+
+v4 <- seq(1, 10, by = 2) # 1, 3, 5, 7, 9
+v4 
+
+v5 <- seq(0, 10, by = 2) # 0, 2, 4, 6, 8, 10
+v5
+
+v2 * v3 * v1
+
+str1 <- "a"
+str1
+
+str2 <- "text"
+str2
+
+str3 <- "Hello world"
+str3
+
+str4 <- c("a", "b", "c")
+str4
+
+str5 <- c("Hello!", "world", "is", "good")
+str5
