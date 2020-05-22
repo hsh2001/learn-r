53ea4277ffa30d1d3a95b4bb57c3bbebef0e9c25
Seung-hyun Hwang <32220765+hsh2001@users.noreply.github.com>
2020-05-22 13:28:06 +0900

    Create HSH-10-1-analysis.R

--- 0011/HSH-10-1-analysis.R
+++ 0011/HSH-10-1-analysis.R
@@ -1,3 +1,5 @@
+install.packages('foreign')
+
 library(foreign)
 library(readxl)
 library(ggplot2)
@@ -88,3 +90,65 @@ ageg_income
 ggplot(data = ageg_income, aes(x = ageg, y = mean_income)) +
   geom_col() +
   scale_x_discrete(limits = c("young", "middle", "old"))
+
+sex_income <- welfare %>%
+  filter(!is.na(income)) %>%
+  group_by(ageg, sex) %>%
+  summarise(mean_income = mean(income))
+
+sex_income
+
+ggplot(data = sex_income, aes(x = ageg, y = mean_income, fill = sex)) + 
+  geom_col() + 
+  scale_x_discrete(limits = c("young", "middle", "old"))
+      
+ggplot(data = sex_income, aes(x = ageg, y = mean_income, fill = sex)) + 
+  geom_col(position = 'stack') + 
+  scale_x_discrete(limits = c("young", "middle", "old"))
+
+ggplot(data = sex_income, aes(x = ageg, y = mean_income, fill = sex)) + 
+  geom_col(position = 'dodge') + 
+  scale_x_discrete(limits = c("young", "middle", "old"))
+
+sex_age <- welfare %>%
+  filter(!is.na(income)) %>%
+  group_by(age, sex) %>%
+  summarise(mean_income = mean(income))
+
+head(sex_age)
+         
+
+ggplot(data = sex_age, aes(x = age, y = mean_income, col = sex)) + 
+  geom_line() 
+
+list_job <- read_excel("0011/Koweps_Codebook.xlsx", col_names = T, sheet = 2)
+head(list_job)
+
+welfare <- left_join(welfare, list_job, id = "code_job")
+
+welfare %>%
+  filter(!is.na(code_job)) %>%
+  select(code_job, job) %>%
+  head(10)
+
+job_income <- welfare %>% 
+  filter(!is.na(job) & !is.na(income)) %>%
+  group_by(job) %>%
+  summarise(mean_income = mean(income))
+
+head(job_income)
+
+top10 <- job_income %>% arrange(desc(mean_income)) %>% head(10)
+top10
+
+low10 <- job_income %>% arrange(mean_income) %>% head(10)
+low10
+
+ggplot(data = top10, aes(x = reorder(job, mean_income), y = mean_income)) + 
+  geom_col() +
+  coord_flip()
+
+ggplot(data = low10, aes(x = reorder(job, -mean_income), y = mean_income)) + 
+  geom_col() +
+  coord_flip() +
+  ylim(0, 900)
